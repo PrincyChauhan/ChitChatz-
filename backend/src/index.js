803c26af3c94124e1992ab090fe0bff8e0717fae
PrincyChauhan <chauhanprincee7@gmail.com>
2025-02-14 14:48:53 +0530

    Mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import "dotenv/config.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
