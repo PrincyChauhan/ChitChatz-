a60eaea8b25cffa4a9883917df917843b770a955
PrincyChauhan <chauhanprincee7@gmail.com>
2025-02-14 17:27:33 +0530

    Update Profile Pic API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import "dotenv/config.js";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
